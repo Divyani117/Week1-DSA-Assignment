bd75a6efe6df9ccf49188fe6315e53758a420bf0
Divyani Singh <2k23.it1a.2310234@gmail.com>
2026-06-23 13:46:12 +0530

    Create bubble_sort.cpp

---
+++ bubble_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int swaps = 0;
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swaps++;
+            }
+        }
+    }
+
+    cout << "Sorted Array:\n";
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    cout << "\nTotal Swaps = " << swaps;
+}
